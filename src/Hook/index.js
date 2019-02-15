c69f1455d68e19131093faac065a6355663181e8
yulong song <yulongsong@yulongs-MacBook-Pro.local>
2019-02-14 23:15:09 -0500

    unfinished test with storybook

---
+++ src/Hook/index.js
@@ -0,0 +1,3 @@
+import Hook from './Hook';
+
+export default Hook;
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,12 +4,14 @@ import { Route, Switch } from 'react-router';
 import TestMe from './TestMe';
 import Login from './Login';
 import PageNotFound from './PageNotFound';
+import Hook from './Hook';
 
 const Routes = (
     <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/test" component={TestMe} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/hook" component={Hook} />
         <Route component={PageNotFound} />
     </Switch>
 );
