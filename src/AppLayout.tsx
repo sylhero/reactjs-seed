8dc741983d6d7b5d1f1d351e648a56b749715227
yulong.song <yulong.song@ormuco.com>
2021-06-29 22:07:20 -0400

    fix

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -4,10 +4,11 @@ import { Layout } from 'antd';
 import LeftSideBar from './LeftSideBar';
 import MainContent from './MainContent';
 import Header from './PageHeader';
+//import Background from './assets/img/background.jpg';
 
 const AppLayout = (): React.ReactElement => {
     return (
-        <Layout style={{ minHeight: '100vh' }}>
+        <Layout style={{ minHeight: '100vh', backgroundImage: 'url("./assets/img/background.jpg")' }}>
             <Layout>
                 <Layout.Content>
                     <MainContent />
