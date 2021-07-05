26b30d1f73f08a41a215026189061e1337d019d9
yulong.song <yulong.song@ormuco.com>
2021-07-05 19:54:32 -0400

    fix

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -5,15 +5,7 @@ import Background from './assets/img/background.jpg';
 
 const AppLayout = (): React.ReactElement => {
     return (
-        <Layout
-            style={{
-                minHeight: `calc(100vh - 3px)`,
-                background: `url(${Background}) no-repeat center center fixed`,
-                backgroundSize: 'cover',
-                width: '100%',
-                height: '100%'
-            }}
-        >
+        <Layout>
             <Layout>
                 <Layout.Content>
                     <MainContent />
