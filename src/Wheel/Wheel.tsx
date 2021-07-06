f27dc59e4d7918f5877dee7cb76ea9a4196e2ef8
Yulong Song <yulong.song@plusgrade.com>
2021-07-06 11:33:41 -0400

    fix

--- src/Wheel/Wheel.tsx
+++ src/Wheel/Wheel.tsx
@@ -49,6 +49,7 @@
 // });
 import React, { useState, useEffect, useRef } from 'react';
 import { LuckyWheel } from 'react-luck-draw';
+import { Fireworks } from 'fireworks/lib/react';
 import outerWheel from '../assets/img/outer-wheel.png';
 import pointer from '../assets/img/pointer2.png';
 import prize from '../assets/img/prize.png';
@@ -57,12 +58,21 @@ import { jsx, css } from '@emotion/react';
 import styles from './Wheel.scss';
 
 const Wheel = (): React.ReactElement => {
-    const [prizes, setPrizes] = useState([]);
+    const [showFirework, setShowFirework] = useState(false);
     const [states, setStates] = useState({ blocks: null, buttons: null, defaultStyle: null, prizes: null });
     const wheel = useRef(null);
     const defaultStyle = {
         fontColor: '#000',
         fontSize: '13px'
+    };
+    const fwConfig = {
+        colors: ['#cc3333', '#F8DEF8', '#81C784'],
+        count: 8,
+        calc: (props, i) => ({
+            ...props,
+            x: (i + 1) * (window.innerWidth / 2) - (i + 1) * 100,
+            y: 400 + Math.random() * 100 - 50 + (i === 2 ? -80 : 0)
+        })
     };
     const defaultConfig = {
         offsetDegree: 22.5
@@ -76,27 +86,6 @@ const Wheel = (): React.ReactElement => {
     const blocks = [{ imgs: [{ src: outerWheel, width: '100%', rotate: true }] }];
     const loadSettings = () => {
         //TODO: fetch settings from BE
-        const tempPrizes = [];
-
-        const data = [
-            { name: '10个京豆', img: prize, color: '#F8DEF8' },
-            { name: '5个京豆', img: prize, color: '#FEF3FC' },
-            { name: '1个京豆', img: prize, color: '#F8DEF8' },
-            { name: '谢谢参与', img: prize, color: '#FEF3FC' },
-            { name: '10个京豆', img: prize, color: '#F8DEF8' },
-            { name: '5个京豆', img: prize, color: '#FEF3FC' },
-            { name: '1个京豆', img: prize, color: '#F8DEF8' },
-            { name: '谢谢参与', img: prize, color: '#FEF3FC' }
-        ];
-        data.forEach((item, index) => {
-            tempPrizes.push({
-                name: item.name,
-                background: item.color,
-                fonts: [{ text: item.name, top: '12%' }],
-                imgs: [{ src: item.img, width: '45%', top: '35%' }]
-            });
-        });
-        setPrizes(tempPrizes);
     };
     useEffect(() => {
         // make call to get list of items
@@ -148,33 +137,38 @@ const Wheel = (): React.ReactElement => {
         });
     }, []);
     return (
-        <div className={styles.wheel}>
-            <LuckyWheel
-                ref={wheel}
-                width="200px"
-                height="200px"
-                blocks={states.blocks}
-                prizes={states.prizes}
-                buttons={states.buttons}
-                defaultStyle={states.defaultStyle}
-                onStart={() => {
-                    // 点击抽奖按钮会触发star回调
-                    // 调用抽奖组件的play方法开始游戏
-                    wheel.current.play();
-                    // 模拟调用接口异步抽奖
-                    setTimeout(() => {
-                        // 假设拿到后端返回的中奖索引
-                        const index = 2; //(Math.random() * 6) >> 0;
-                        // 调用stop停止旋转并传递中奖索引
-                        wheel.current.stop(index);
-                    }, 2500);
-                }}
-                onEnd={(prize) => {
-                    // 抽奖结束会触发end回调
-                    console.log(prize);
-                    alert('恭喜获得大奖:' + prize.title);
-                }}
-            ></LuckyWheel>
+        <div>
+            <div>{showFirework && <Fireworks {...fwConfig} />}</div>
+            <div className={styles.wheel}>
+                <LuckyWheel
+                    ref={wheel}
+                    width="200px"
+                    height="200px"
+                    blocks={states.blocks}
+                    prizes={states.prizes}
+                    buttons={states.buttons}
+                    defaultStyle={states.defaultStyle}
+                    onStart={() => {
+                        setShowFirework(false);
+                        // 点击抽奖按钮会触发star回调
+                        // 调用抽奖组件的play方法开始游戏
+                        wheel.current.play();
+                        // 模拟调用接口异步抽奖
+                        setTimeout(() => {
+                            // 假设拿到后端返回的中奖索引
+                            const index = 2; //(Math.random() * 6) >> 0;
+                            // 调用stop停止旋转并传递中奖索引
+                            wheel.current.stop(index);
+                        }, 2500);
+                    }}
+                    onEnd={(prize) => {
+                        // 抽奖结束会触发end回调
+                        console.log(prize);
+                        setShowFirework(true);
+                        //alert('恭喜获得大奖:' + prize.title);
+                    }}
+                ></LuckyWheel>
+            </div>
         </div>
     );
 };
